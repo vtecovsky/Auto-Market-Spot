4fee517eb3a6766094bd1fee878a0b418a5b9554
Saveliy <s.lekhtin@innopolis.university>
2023-05-18 12:58:38 +0300

    Настройка файловой структуры

--- migrations/env.py
+++ migrations/env.py
@@ -5,8 +5,15 @@ from sqlalchemy import pool
 
 from alembic import context
 
-from config import DB_HOST, DB_PORT, DB_USER, DB_NAME, DB_PASS
-from auth.models import metadata
+import os
+import sys
+
+sys.path.append(os.path.join(sys.path[0], 'src'))
+
+
+from src.config import DB_HOST, DB_PORT, DB_USER, DB_NAME, DB_PASS
+from src.auth.models import metadata as metadata_auth
+from src.items.models import metadata as metadata_items
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -34,7 +41,7 @@ if config.config_file_name is not None:
 """
 metadata, которая отвечает за таблицы
 """
-target_metadata = metadata
+target_metadata = [metadata_auth, metadata_items]
 
 
 # other values from the config, defined by the needs of env.py,
