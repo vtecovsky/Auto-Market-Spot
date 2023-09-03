e425cd3f0395feddc55e1461f9a98bba862e6f56
Saveliy <s.lekhtin@innopolis.university>
2023-09-03 15:13:28 +0300

    small changes

--- src/items/models.py
+++ src/items/models.py
@@ -8,5 +8,5 @@ class Item(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     price = Column(Float, index=True)
-    figi = Column(String, index=True)
+    description = Column(String, index=True)
     type = Column(String, index=True)
--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 
+from src.database import engine, Base
 from src.auth.base_config import auth_backend, fastapi_users
 from src.auth.schemas import UserCreate, UserRead
 from src.routers import routers
@@ -19,13 +20,13 @@ app.include_router(
 )
 
 
+
 # To clean the database
 # @app.on_event("startup")
 # async def init_tables():
 #     async with engine.begin() as conn:
 #         await conn.run_sync(Base.metadata.drop_all)
 #         await conn.run_sync(Base.metadata.create_all)
-
 
 for router in routers:
     app.include_router(router)
